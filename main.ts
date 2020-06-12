d60c5a2e11b16188f16b40c8574e2bf6c6eeb935
Amanda Grice <amandalyngrice@gmail.com>
2020-06-11 22:30:39 -0400

    add monkey

--- main.ts
+++ main.ts
@@ -13,6 +13,7 @@ enum ActionKind {
 namespace SpriteKind {
     export const Tree = SpriteKind.create()
     export const Sword = SpriteKind.create()
+    export const Monkey = SpriteKind.create()
 }
 namespace myTiles {
     //% blockIdentity=images._tile
@@ -245,6 +246,13 @@ function SetPlayerAnimations () {
 `)
     animation.attachAnimation(playerSprite, animWalkLeft)
 }
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    isSwordVisible = 1
+    sword.setFlag(SpriteFlag.Invisible, false)
+    pause(100)
+    sword.setFlag(SpriteFlag.Invisible, true)
+    isSwordVisible = 0
+})
 function SetSnakeAnimations () {
     snakeSprite = sprites.create(img`
 . . . . c c c c c c . . . . . . 
@@ -422,16 +430,10 @@ sprites.onOverlap(SpriteKind.Sword, SpriteKind.Enemy, function (sprite, otherSpr
         otherSprite.destroy(effects.fire, 200)
     }
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    isSwordVisible = 1
-    sword.setFlag(SpriteFlag.Invisible, false)
-    pause(100)
-    sword.setFlag(SpriteFlag.Invisible, true)
-    isSwordVisible = 0
-})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     animation.setAction(otherSprite, ActionKind.snakeAttack)
     info.changeLifeBy(-1)
+    scene.cameraShake(4, 500)
     pause(2000)
 })
 function CalculateSwordPlacement () {
@@ -538,6 +540,24 @@ sword = sprites.create(img`
 . . . . . . 4 4 4 4 . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Sword)
+let monkey = sprites.create(img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`, SpriteKind.Monkey)
 sword.setFlag(SpriteFlag.Invisible, true)
 controller.moveSprite(playerSprite)
 SetPlayerAnimations()
@@ -623,7 +643,84 @@ c c 7 7 7 7 7 7 7 7 7 7 7 7 6 c
 `, SpriteKind.Tree)
     tiles.placeOnTile(treeSprite, value)
 }
+animation.runImageAnimation(
+monkey,
+[img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`,img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+c d e e d d d d e e d d f . . . 
+c d d d d c d d e e b d c . . . 
+c c c c c d d e e e b d c . f f 
+. f d d d d e e e f f c . f e f 
+. f f f f f f e e e e f . f e f 
+. f f f f e e e e e e e f f e f 
+f d d f d d f e f e e e e f f . 
+f d b f d b f e f e e e e f . . 
+f f f f f f f f f f f f e f . . 
+. . . . . . . . . f c d d f . . 
+. . . . . . . . . . f f f f . . 
+`,img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f f . . . . 
+. c d d d d d d e e d d f . . . 
+. c d f d d f d e e b d c . . . 
+c d d f d d f d e e b d c . f f 
+c d e e d d d d e e f c . f e f 
+c d d d d c d e e e f . . f e f 
+. f c c c d e e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f f . 
+. . f f e f e e f e e e e f . . 
+. f e f f e e f f f e e e f . . 
+f d d b d d c f f f f f f b f . 
+f d d c d d d f . . f c d d f . 
+. f f f f f f f . . . f f f . . 
+`,img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f f f . . . . 
+. . f d d d e e e e d d f . . . 
+. c d d d d d e e e b d c . . . 
+. c d d d d d d e e b d c . . . 
+c d d f d d f d e e f c . f f . 
+c d d f d d f d e e f . . f e f 
+c d e e d d d d e e f . . f e f 
+. f d d d c d e e f f . . f e f 
+. . f f f d e e e e e f . f e f 
+. . . . f e e e e e e e f f f . 
+. . . . f f e e e e e b f f . . 
+. . . f e f f e e c d d f f . . 
+. . f d d b d d c f f f . . . . 
+. . f d d c d d d f f . . . . . 
+. . . f f f f f f f . . . . . . 
+`],
+200,
+true
+)
 game.onUpdate(function () {
     CalculateSwordPlacement()
     TriggerPlayerAnimations()
+    if (Math.percentChance(1)) {
+        monkey.setVelocity(Math.randomRange(-10, 10), Math.randomRange(-10, 10))
+    }
 })
